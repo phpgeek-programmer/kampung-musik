9ed98f73539b239332ee9631c8de5f7ae521cd7c
phpgeek-programmer <phpgeek.programmer@gmail.com>
2013-10-23 15:09:51 +0700

    add Sentry configuration to app/config/app.php

--- app/config/app.php
+++ app/config/app.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'url' => 'http://localhost',
+	'url' => 'http://dev.kampungmusik.local',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -113,6 +113,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+                'Cartalyst\Sentry\SentryServiceProvider',
 
 	),
 
@@ -178,6 +179,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+                'Sentry' => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
 
 	),
 
